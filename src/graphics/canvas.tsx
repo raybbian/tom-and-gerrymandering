bd096519f1d71b195285d3176941883ee961977f
Raymond Bian <raybbian@gmail.com>
2024-09-29 01:07:46 -0400

    Selection vis, adjust foliage color, prep district selection to use voter majority

--- src/graphics/canvas.tsx
+++ src/graphics/canvas.tsx
@@ -6,31 +6,32 @@ import {
     GridGenerator,
     HalfEdge,
 } from "@/scripts/grid";
-import {
-    CameraControls,
-    Line,
-    PerspectiveCamera,
-    Stats,
-} from "@react-three/drei";
+import { CameraControls, PerspectiveCamera, Stats } from "@react-three/drei";
 import { GridSpace } from "./grid_space";
 import { GameState } from "@/scripts/game_state";
 import { useEffect, useMemo, useRef, useState } from "react";
+import { ConvexGeometry } from "three/examples/jsm/Addons.js";
 
 const highCutoff = 0.73;
 const medCutoff = 0.5;
 
-function Buildings({grid} : {grid: GridGenerator}): React.ReactNode {
+function Buildings({ grid }: { grid: GridGenerator }): React.ReactNode {
     const { scene } = useThree();
 
     useEffect(() => {
         const radius = (grid.hexagonSideLen - 1) * grid.unitLen;
-        const num = Math.pow(radius,2) * 2000;
+        const num = Math.pow(radius, 2) * 2000;
         const batched = new THREE.BatchedMesh(num * 10, num * 10 * 216);
 
-
-        const towerId = batched.addGeometry(new THREE.BoxGeometry(0.05, 0.2, 0.05));
-        const houseId = batched.addGeometry(new THREE.BoxGeometry(0.03, 0.05, 0.03));
-        const treeId1 = batched.addGeometry(new THREE.ConeGeometry(0.018, 0.06));
+        const towerId = batched.addGeometry(
+            new THREE.BoxGeometry(0.05, 0.2, 0.05),
+        );
+        const houseId = batched.addGeometry(
+            new THREE.BoxGeometry(0.03, 0.05, 0.03),
+        );
+        const treeId1 = batched.addGeometry(
+            new THREE.ConeGeometry(0.018, 0.06),
+        );
         const treeId2 = batched.addGeometry(new THREE.ConeGeometry(0.025, 0.1));
 
         for (let i = 0; i < num; i++) {
@@ -51,7 +52,12 @@ function Buildings({grid} : {grid: GridGenerator}): React.ReactNode {
             }
 
             let ii = 0;
-            const noise = GameState.perlinPopulation.getNormalizedNoise(x, y, 0, 1);
+            const noise = GameState.perlinPopulation.getNormalizedNoise(
+                x,
+                y,
+                0,
+                1,
+            );
             if (noise > highCutoff) {
                 if (Math.random() > noise) continue;
                 else ii = batched.addInstance(towerId);
@@ -59,28 +65,35 @@ function Buildings({grid} : {grid: GridGenerator}): React.ReactNode {
                 let scale = (noise - highCutoff) / (1 - highCutoff) + 0.5;
                 if (Math.random() > 0.9) scale *= 2.0;
 
-                const yo = scale * 0.2 / 2;
+                const yo = (scale * 0.2) / 2;
                 batched.setMatrixAt(
                     ii,
                     new THREE.Matrix4().compose(
                         new THREE.Vector3(x, yo, y),
                         new THREE.Quaternion(),
-                        new THREE.Vector3(Math.random() * 0.2 + 0.9, scale, Math.random() * 0.2 + 0.9),
+                        new THREE.Vector3(
+                            Math.random() * 0.2 + 0.9,
+                            scale,
+                            Math.random() * 0.2 + 0.9,
+                        ),
                     ),
                 );
             } else if (noise > medCutoff) {
                 if (Math.random() > noise - 0.23) {
                     if (Math.random() < 0.3) ii = batched.addInstance(treeId1);
                     else continue;
-                }
-                else ii = batched.addInstance(houseId);
+                } else ii = batched.addInstance(houseId);
 
                 batched.setMatrixAt(
                     ii,
                     new THREE.Matrix4().compose(
                         new THREE.Vector3(x, 0, y),
                         new THREE.Quaternion(),
-                        new THREE.Vector3(Math.random() * 0.2 + 0.9, Math.random() * 0.6 + 0.8, Math.random() * 0.2 + 0.9),
+                        new THREE.Vector3(
+                            Math.random() * 0.2 + 0.9,
+                            Math.random() * 0.6 + 0.8,
+                            Math.random() * 0.2 + 0.9,
+                        ),
                     ),
                 );
             } else {
@@ -103,13 +116,16 @@ function Buildings({grid} : {grid: GridGenerator}): React.ReactNode {
                 }
             }
         }
-        const mat = new THREE.MeshStandardMaterial();
-        mat.color = new THREE.Color(0.1, 0.15, 0.15);
+        const mat = new THREE.MeshStandardMaterial({
+            opacity: 0.5,
+            transparent: true,
+        });
+        mat.color = new THREE.Color(1, 1, 1);
         batched.material = mat;
         scene.add(batched);
         return () => {
             scene.remove(batched);
-        }
+        };
     }, [grid, scene]);
 
     return <></>;
@@ -144,14 +160,17 @@ export default function GridCanvas({
             const cell = gameState.cells[val];
             const POPULATION_SCALE = 10000;
             setDistrictInfo([
-                ~~(cell.truePopulation * POPULATION_SCALE / 100) * 100,
+                ~~((cell.truePopulation * POPULATION_SCALE) / 100) * 100,
                 Math.round(cell.voterProportion * 100),
                 Math.round((1 - cell.voterProportion) * 100),
             ]);
             // console.log("setting info");
         }
         if (currentSelection.current != null && mouseDown.current) {
-            if (gameState.actionMode == "redistricting" && startingSelection.current! <= gameState.maxDistricts) {
+            if (
+                gameState.actionMode == "redistricting" &&
+                startingSelection.current! <= gameState.maxDistricts
+            ) {
                 gameState.addCellToDistrict(
                     currentSelection.current,
                     startingSelection.current,
@@ -181,24 +200,33 @@ export default function GridCanvas({
         mouseDown.current = val;
     }
 
-    const buildingsComp = useMemo(() => (
-        <Buildings
-            grid={grid}
-        />
-    ), [grid]);
+    const buildingsComp = useMemo(() => <Buildings grid={grid} />, [grid]);
 
     const borderLines = useMemo(() => {
-        const mp: Map<HalfEdge, [THREE.Vector3, THREE.Vector3]> = new Map();
+        const LINE_WIDTH = 0.01;
+        const mp: Map<HalfEdge, THREE.Vector3[]> = new Map();
         grid.dcel.halfEdges.forEach((he) => {
-            if (mp.has(he) || mp.has(he.twin)) {
+            if (mp.has(he.twin)) {
                 return;
             }
-            const linePoints: [THREE.Vector3, THREE.Vector3] = [
-                new THREE.Vector3(he.a.pos[0], 0, he.a.pos[1]),
-                new THREE.Vector3(he.b.pos[0], 0, he.b.pos[1]),
+            const aToB = [he.b.pos[0] - he.a.pos[0], he.b.pos[1] - he.a.pos[1]];
+            const bToLeft = [-aToB[1], aToB[0]];
+            const len = Math.sqrt(Math.pow(aToB[0], 2) + Math.pow(aToB[1], 2));
+            bToLeft[0] /= len / LINE_WIDTH;
+            bToLeft[1] /= len / LINE_WIDTH;
+
+            const rect = [
+                [he.a.pos[0], he.a.pos[1]],
+                [he.b.pos[0], he.b.pos[1]],
+                [he.a.pos[0] + bToLeft[0], he.a.pos[1] + bToLeft[1]],
+                [he.b.pos[0] + bToLeft[0], he.b.pos[1] + bToLeft[1]],
             ];
-            mp.set(he, linePoints);
-            mp.set(he.twin, linePoints);
+
+            const points = [
+                ...rect.map(([x, z]) => new THREE.Vector3(x, 0, z)),
+                ...rect.map(([x, z]) => new THREE.Vector3(x, 0.001, z)),
+            ];
+            mp.set(he.twin, points);
         });
         return mp;
     }, [grid]);
@@ -213,18 +241,9 @@ export default function GridCanvas({
                         new THREE.Vector3(point.pos[0], -0.1, point.pos[1]),
                 ),
                 ...pointList.map(
-                    (point) =>
-                        new THREE.Vector3(point.pos[0], -0.0001, point.pos[1]),
+                    (point) => new THREE.Vector3(point.pos[0], 0, point.pos[1]),
                 ),
             ]);
-        });
-        return mp;
-    }, [grid]);
-
-    const districtColors = useMemo(() => {
-        const mp: Map<number, number> = new Map();
-        Array.from(grid.dcel.faces.values()).forEach((_, i) => {
-            mp.set(i, Math.random() * 0xffffff);
         });
         return mp;
     }, [grid]);
@@ -247,9 +266,7 @@ export default function GridCanvas({
     }, []);
 
     return (
-        <Canvas
-            onPointerOut={() => setCurrentSelection(null)}
-        >
+        <Canvas onPointerOut={() => setCurrentSelection(null)}>
             <ambientLight intensity={Math.PI / 2} />
             <CameraControls
                 ref={cameraControlRef}
@@ -288,15 +305,40 @@ export default function GridCanvas({
                         (cellInd) => gameState.cells[cellInd].dcelFace,
                     );
                     const shell = exteriorHEOfFaces(faces);
-                    return shell.map((he, i) => {
-                        return (
-                            <Line
-                                key={i}
-                                points={borderLines.get(he)!}
-                                color={districtColors.get(districtInd)}
-                            />
-                        );
-                    });
+                    return [
+                        ...shell.map((he, i) => {
+                            return (
+                                <mesh
+                                    key={i}
+                                    geometry={
+                                        new ConvexGeometry(borderLines.get(he)!)
+                                    }
+                                >
+                                    <meshStandardMaterial color={"hotpink"} />
+                                </mesh>
+                            );
+                        }),
+                        ...faces.map((face, i) => {
+                            return (
+                                <mesh
+                                    key={`h${i}`}
+                                    geometry={
+                                        new ConvexGeometry(
+                                            gridPrisms.get(face)!,
+                                        )
+                                    }
+                                    scale={[1, 0.01, 1]}
+                                    position={[0, 0.001, 0]}
+                                >
+                                    <meshStandardMaterial
+                                        color={"hotpink"}
+                                        opacity={0.2}
+                                        transparent
+                                    />
+                                </mesh>
+                            );
+                        }),
+                    ];
                 },
             )}
             {Array.from(grid.dcel.faces.values()).map((face, i) => {
